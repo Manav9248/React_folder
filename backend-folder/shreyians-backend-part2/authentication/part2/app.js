6d32e38bacb9da12fe6e49896e8b72a6cf48c602
Manav Pal <manav96544@gmail.com>
2025-01-31 22:14:21 +0530

    part2

--- backend-folder/shreyians-backend-part2/authentication/part2/app.js
+++ backend-folder/shreyians-backend-part2/authentication/part2/app.js
@@ -1,2 +1,29 @@
 const express = require('express');
 const app = express();
+
+// setting view engine for ejs
+app.set("view engine","ejs");
+
+//middleware using
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use()
+
+
+
+app.get('/',(req,res)=>{
+    res.send("Route Page")
+})
+
+
+
+
+
+
+
+
+
+app.listen("3000",()=>{
+    console.log("Port is running on 3000")
+})
